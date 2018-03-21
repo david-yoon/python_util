06b86eb463ed6c98bcf7d1353a0bfaaed21bdba0
dato <mysmilesh@gmail.com>
2018-03-21 14:43:25 +0900

    create folder

--- file_util.py
+++ file_util.py
@@ -39,3 +39,9 @@ def find_encoding(filename):
     charenc = result['encoding']    
     return charenc
             
+'''
+dir_name : dir_name (inc. path) that will be created ( full-path name )
+'''
+def create_folder(dir_name):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
