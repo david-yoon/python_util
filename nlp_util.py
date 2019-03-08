5f435d219d2a379bb87c4a3d01364f6de52731a2
dato <mysmilesh@gmail.com>
2019-03-08 16:11:16 +0900

    modify mincut func (python 3.x)

--- nlp_util.py
+++ nlp_util.py
@@ -2,25 +2,56 @@
 
 
 '''
-dic           : original dictionary
+dic        : original dictionary
 min_freq   : cut-off frequenccy that will be removed from the dictionary, ex) min_freq=0 --> all data
-return       : new_dictionary    
+return     : new_dictionary    
 '''
-def apply_mincut(dic, min_freq):
+def apply_mincut_lessequal_than(dic, min_freq):
 
-    print 'apply minCut and re-generate minCutDic'
-    mincut_dic = dict(filter(lambda (a, b) : b > min_freq, dic.items()))
+    mincut_dic = {}
+    print ('apply minCut and re-generate minCutDic')
+    
+    for key in dic.keys():
+        if dic[key] > min_freq:
+            mincut_dic[key] = dic[key]
 
-    print 'minFreq = ' + str(min_freq)
-    print 'original dic size = ' + str(len(dic))
-    print 'original dic word freq = ' + str(sum(dic.values()))
-    print 'minCut dic size = ' + str(len(mincut_dic))
-    print 'minCut dic word freq = ' + str(sum(mincut_dic.values()))
+    print ('minFreq = ' + str(min_freq))
+    print ('original dic size = ' + str(len(dic)))
+    print ('original dic word freq = ' + str(sum(dic.values())))
+    print ('minCut dic size = ' + str(len(mincut_dic)))
+    print ('minCut dic word freq = ' + str(sum(mincut_dic.values())))
 
     coverage = sum(mincut_dic.values()) / float(sum(dic.values()))
-    print 'coverage = ' + str(coverage)
+    print ('coverage = ' + str(coverage))
 
     return mincut_dic
+
+
+
+
+'''
+deprecated
+dic        : original dictionary
+min_freq   : cut-off frequenccy that will be removed from the dictionary, ex) min_freq=0 --> all data
+return     : new_dictionary    
+'''
+"""
+def apply_mincut(dic, min_freq):
+
+    print ('apply minCut and re-generate minCutDic')
+    mincut_dic = dict(filter(lambda (a, b) : b > min_freq, dic.items()))
+
+    print ('minFreq = ' + str(min_freq))
+    print ('original dic size = ' + str(len(dic)))
+    print ('original dic word freq = ' + str(sum(dic.values())))
+    print ('minCut dic size = ' + str(len(mincut_dic)))
+    print ('minCut dic word freq = ' + str(sum(mincut_dic.values())))
+
+    coverage = sum(mincut_dic.values()) / float(sum(dic.values()))
+    print ('coverage = ' + str(coverage))
+
+    return mincut_dic
+"""
 
 
 '''
@@ -41,6 +72,6 @@ list_index  :
 return       : print sentence
 '''
 def index_to_sentence( inv_dic, list_index ) :
-    print [ inv_dic[ x ] for x in list_index ]
+    print ([ inv_dic[ x ] for x in list_index ])
 
 
