ccd83ed8f1b7593f4139c821faecdc3f469d1e99
dato <mysmilesh@gmail.com>
2019-03-29 12:46:43 +0900

    [bugfix] resolving y_score has same values

--- measure_QA.py
+++ measure_QA.py
@@ -19,17 +19,36 @@ def MAP(y_true, y_score):
     map = float(0)
     map_idx = 0
     extracted = []
+    
+    # check duplicate - wrong prediction
+    tmp_check = set(y_score)
+    if (len(y_score) - len(tmp_check)) > 3:
+        print ("WARN same predictions")
 
     for i in range( len(ground_label) ):
         if ground_label[i] != 0 :
             extracted.append(i)
 
+    # for resolving same number
     sss = sorted( y_score, reverse=True )
-    sss_rank = [ list( y_score).index(x) for x in sss]
+    #sss_rank = [ list( y_score).index(x) for x in sss]
+    
+    # for resolving same number
+    sss_rank = []
+    for x in sss:
+        list_index = ([i for i, val in enumerate(y_score) if val==x])
+        if( len(list_index) == 1 ):
+            sss_rank.append( list_index[0] )
+        else:
+            for i in list_index:
+                if i not in sss_rank:
+                    sss_rank.append(i)
+                    break    
+    
     predict_label = np.asarray(sss_rank)
     
-    #print predict_label
-    #print extracted
+#     print (extracted)
+#     print (predict_label)
     
     for i in range( len(predict_label) ) :
         if predict_label[i] in extracted :
@@ -41,7 +60,26 @@ def MAP(y_true, y_score):
     else:
         map = 0
     
+    '''
+    with open('./TEST_log_map_results.txt', 'a') as f:
+        # label index
+        str_extracted = [str(x) for x in extracted]
+        f.write( ' '.join(str_extracted) )
+        f.write('\n')
+
+        # prediction (rank)
+        str_predict_label = [str(x) for x in predict_label]
+        f.write( ' '.join(str_predict_label) )
+        f.write('\n')
+
+        # prediction (prob.)
+        str_y_score = [str(x) for x in y_score]
+        f.write( ' '.join(str_y_score) )
+        f.write('\n\n')
+    '''
+    
     return map
+
 
 
 def MRR(y_true, y_score):
@@ -58,11 +96,22 @@ def MRR(y_true, y_score):
             extracted.append(i)
 
     sss = sorted( y_score, reverse=True )
-    sss_rank = [ list( y_score).index(x) for x in sss]
-    predict_label = np.asarray(sss_rank)
+    #sss_rank = [ list( y_score).index(x) for x in sss]
     
-    #print predict_label
-    #print extracted
+    # for resolving same number
+    sss_rank = []
+    for x in sss:
+        list_index = ([i for i, val in enumerate(y_score) if val==x])
+        if( len(list_index) == 1 ):
+            sss_rank.append( list_index[0] )
+        else:
+            for i in list_index:
+                if i not in sss_rank:
+                    sss_rank.append(i)
+                    break    
+    
+    
+    predict_label = np.asarray(sss_rank)
     
     for i in range( len(predict_label) ) :
         if predict_label[i] in extracted :
